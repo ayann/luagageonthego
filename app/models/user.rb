5f662f36bdd7387a8cee6c18d62dd777f3926df9
Armand Niampa <armand.niampa@gmail.com>
2015-10-08 13:48:41 +0200

    Locale for french rails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :google_authenticatable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+	validates :last_name, :first_name, presence: true
+
+	after_save do
+		self.pseudo = "#{first_name[0]}.#{last_name}"
+	end
 end
